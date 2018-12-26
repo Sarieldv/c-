0e774e847a14b35e87cef0b3e808b1e6c2845c4f
sasegal <sasegal@B015-53-57>
2018-12-26 14:19:12 +0200

    address

---
+++ Project01_3165_6269_dotNet5779_1/address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project01_3165_6269_dotNet5779_1
+{
+    class Address
+    {
+        private string StreetName;
+        private int AddressNumber;
+        private string CityName;
+    }
+}
