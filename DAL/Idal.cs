04d6770bba98b21fbe1e11fc5d1aee54ee2e5a55
sarieldv <sarieldv@gmail.com>
2018-12-26 21:01:49 +0200

    started Dal

---
+++ DAL/Idal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    interface Idal
+    {
+        void AddTester();
+        void EraseTester();
+        void UpdateTester();
+
+    }
+}
